1eaaa08bfc92e5974130f8d37ebf441065ae5e88
SVYATOSLAV ZALUTSKYY <124689362+SponeKiller@users.noreply.github.com>
2025-10-05 13:48:04 +0200

    issue fixed

--- app/api/v1/schemas/user.py
+++ app/api/v1/schemas/user.py
@@ -1,10 +1,6 @@
-from typing import Dict, List
 from datetime import datetime
 
 from pydantic import BaseModel
-
-class UserModuleOut(BaseModel):
-    user_modules: Dict[str, List[str]]
 
 class UserModuleIn(BaseModel):
     module_id: int
